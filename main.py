e6e7d786dc8764a35b8854afea2bf91a3175298c
lAzyCoDeR251 <98582920+lAzyCoDeR251@users.noreply.github.com>
2024-04-05 20:53:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["https://charts-frontend.onrender.com"],
     allow_credentials=True,
     allow_methods=["GET", "POST"],
     allow_headers=["*"],
